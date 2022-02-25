7cba46575dba51c0e0134b00c916b0f645156a45
O4FDev <luke.lucas@ou.ac.uk>
2022-02-25 20:58:45 +0000

    Stole my own styles from PB

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { MenuIcon } from '@heroicons/react/outline'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { useState } from "react";
 
 const Links = [
   {
@@ -21,27 +22,73 @@ const Links = [
 ]
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <header className="p-12 font-semibold">
-      <nav className="flex justify-between items-center">
-        <h1 className="text-2xl">
-          <Link href="/">Luke Lucas</Link>
-        </h1>
-        <ul className="hidden md:flex md:items-center md:gap-4 md:text-[1rem]">
-          {
-            Links.map(link => (
-              <li key={link.name}>
-                <Link href={link.link}>
-                  <a>{link.name}</a>
-                </Link>
-              </li>
-            ))
-          }
-        </ul>
-        <MenuIcon className="w-5 h-5 md:hidden" />
-      </nav>
-    </header>
-  )
+      <div>
+          <div className="flex flex-row justify-between p-12">
+              <div className="flex flex-row items-center">
+                  <h1 className="text-2xl font-semibold">Luke Lucas</h1>
+              </div>
+              <div className="flex flex-row items-center">
+                  <MenuIcon className="h-5 w-5 sticky md:hidden right-12 top-12" 
+                  onClick={() => {
+                      setMenuOpen(!menuOpen)
+                  }}
+                  />
+                  <ul className="hidden md:flex gap-4 text-[16px] font-semibold">
+                    {Links.map((link, index) => {
+                      return (
+                        <li key={index}>
+                          <Link href={link.link}>
+                            <a className="text-sm">{link.name}</a>
+                          </Link>
+                        </li>
+                      )})
+                    }
+                  </ul>
+              </div>
+          </div>
+          {menuOpen && (
+              <div className="flex flex-col items-center justify-center absolute top-0 right-0 w-screen lg:w-2/5 h-full bg-white shadow-xl md:hidden">
+                  <div className="flex flex-row justify-between items-center">
+                      <div className="flex flex-row items-center">
+                          <XIcon className="h-5 w-5 absolute right-12 top-12"
+                          onClick={() => {
+                              setMenuOpen(!menuOpen)
+                          }}
+                          />
+                      </div>
+                  </div>
+                  <div>
+                      <div className="">
+                          <div className="flex flex-col gap-4 font-bold text-left text-2xl lg:text-4xl lg:gap-8">
+                            {
+                              Links.map((link, index) => {
+                                return (
+                                  <div key={index}>
+                                    <Link href={link.link}>
+                                      <a className="">{link.name}</a>
+                                    </Link>
+                                  </div>
+                                )
+                              })
+                            }
+                          </div>
+                      </div>
+                      <div className="flex py-6 flex-row text-lg">
+                          <div className="flex flex-col gap-4 font-bold text-left">
+                              <a className="text-orange-500 underline" href="mailto:luke@rainoto.com
+                              ">
+                                  luke@rainoto.com
+                              </a>
+                              
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          )}
+      </div>
+  );
 };
 
 const Home = () => {
